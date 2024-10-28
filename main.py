2be05859972809974d4d8afb10d5a536601ca3d7
Martí Espinosa <141586888+martiespinosa@users.noreply.github.com>
2024-10-28 17:24:21 +0100

    started

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from microbit import *
 
+def mostrar_temperatura():
+    temperatura_actual = input.temperature()
+    temperatura_maxima = 30
+    basic.clear_screen()
+    led.plot_bar_graph(temperatura_actual, temperatura_maxima)
+
+mostrar_temperatura()
--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
+function mostrar_temperatura() {
+    let temperatura_actual = input.temperature()
+    let temperatura_maxima = 30
+    basic.clearScreen()
+    led.plotBarGraph(temperatura_actual, temperatura_maxima)
+}
 
+mostrar_temperatura()
