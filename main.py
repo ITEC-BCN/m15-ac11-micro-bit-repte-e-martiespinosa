8bd83602f0d374f00a0aafc2bdcb38c23b725141
Martí Espinosa <141586888+martiespinosa@users.noreply.github.com>
2024-10-28 17:43:10 +0100

    no va

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from microbit import *
 
+x = 0
+y = 0
+
 def on_forever():
     temperatura_actual = input.temperature()
     temperatura_maxima = 30
     basic.clear_screen()
     led.plot_bar_graph(temperatura_actual, temperatura_maxima)
+
+    img = images.create_image("""
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    img.set_pixel(x, y, True)
+    img.show_image(0)
+
+    acc_x = input.acceleration(Dimension.X)
+    acc_y = input.acceleration(Dimension.Y)
+
+    if acc_x <= 150 and x > 0:
+        x -= 1
+    if acc_x > 150 and x < 4:
+        x += 1
+    if acc_y <= 150 and y > 0:
+        y -= 1
+    if acc_y > 150 and y < 4:
+        y += 1
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,35 @@
+let x = 0
+let y = 0
 basic.forever(function on_forever() {
     let temperatura_actual = input.temperature()
     let temperatura_maxima = 30
     basic.clearScreen()
     led.plotBarGraph(temperatura_actual, temperatura_maxima)
+    let img = images.createImage(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    img.setPixel(x, y, true)
+    img.showImage(0)
+    let acc_x = input.acceleration(Dimension.X)
+    let acc_y = input.acceleration(Dimension.Y)
+    if (acc_x <= 150 && x > 0) {
+        let x -= 1
+    }
+    
+    if (acc_x > 150 && x < 4) {
+        x += 1
+    }
+    
+    if (acc_y <= 150 && y > 0) {
+        let y -= 1
+    }
+    
+    if (acc_y > 150 && y < 4) {
+        y += 1
+    }
+    
 })
