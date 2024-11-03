6ff9ba5b60ba115cae40040ccfebb70f760129d6
Martí Espinosa <141586888+martiespinosa@users.noreply.github.com>
2024-11-03 01:53:38 +0100

    done

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 from microbit import *
 
+modo = ""
 x = 0
 y = 0
 
-def on_forever():
+def on_button_pressed_a():
+    global modo
+    modo = "estacio_meteorologica"
+
+def on_button_pressed_b():
+    global modo
+    modo = "moure_la_gota"
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def estacio_meteorologica():
     temperatura_actual = input.temperature()
     temperatura_maxima = 50
     basic.clear_screen()
     led.plot_bar_graph(temperatura_actual, temperatura_maxima)
 
+def moure_la_gota():
+    global x, y
+
     img = images.create_image("""
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
     img.set_pixel(x, y, True)
     img.show_image(0)
 
     acc_x = input.acceleration(Dimension.X)
     acc_y = input.acceleration(Dimension.Y)
 
-    global x, y
     if acc_x <= 150 and x > 0:
         x -= 1
     if acc_x > 150 and x < 4:
@@ -32,4 +46,9 @@ def on_forever():
     if acc_y > 150 and y < 4:
         y += 1
 
+def on_forever():
+    if modo == "estacio_meteorologica":
+        estacio_meteorologica()
+    elif modo == "moure_la_gota":
+        moure_la_gota()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,22 +1,34 @@
+let modo = ""
 let x = 0
 let y = 0
-basic.forever(function on_forever() {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    modo = "estacio_meteorologica"
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    modo = "moure_la_gota"
+})
+function estacio_meteorologica() {
     let temperatura_actual = input.temperature()
     let temperatura_maxima = 50
     basic.clearScreen()
     led.plotBarGraph(temperatura_actual, temperatura_maxima)
+}
+
+function moure_la_gota() {
+    
     let img = images.createImage(`
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     img.setPixel(x, y, true)
     img.showImage(0)
     let acc_x = input.acceleration(Dimension.X)
     let acc_y = input.acceleration(Dimension.Y)
-    
     if (acc_x <= 150 && x > 0) {
         x -= 1
     }
@@ -31,6 +43,15 @@ basic.forever(function on_forever() {
     
     if (acc_y > 150 && y < 4) {
         y += 1
+    }
+    
+}
+
+basic.forever(function on_forever() {
+    if (modo == "estacio_meteorologica") {
+        estacio_meteorologica()
+    } else if (modo == "moure_la_gota") {
+        moure_la_gota()
     }
     
 })
